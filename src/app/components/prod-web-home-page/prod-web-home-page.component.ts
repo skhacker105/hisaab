ec73752ee52c28ba8d5cded76a2f226d52e476c4
skhacker105 <skhacker105@gmail.com>
2025-05-11 18:27:15 +0530

    added images to install steps

--- src/app/components/prod-web-home-page/prod-web-home-page.component.ts
+++ src/app/components/prod-web-home-page/prod-web-home-page.component.ts
@@ -10,13 +10,15 @@ export class ProdWebHomePageComponent {
   activeTab: 'install' | 'usage' | 'about' = 'install';
 
   installSteps = [
-    { text: 'Open Play Store', img: '' },
-    { text: 'Click your account icon (top-right)', img: '' },
-    { text: 'Click "Play Protect"', img: '' },
-    { text: 'Click settings (top-right) and disable both toggles', img: '' },
+    { text: 'Open Play Store', img: '1.jpeg' },
+    { text: 'Click your account icon (top-right)', img: '2.jpeg' },
+    { text: 'Click "Play Protect"', img: '3.jpeg' },
+    { text: 'Click settings (top-right)', img: '4.jpeg' },
+    { text: 'Disable both toggles', img: '5.jpeg' },
+    { text: '', img: '6.jpeg' },
     { text: 'Download the app and click Install', img: '', file: {type: 'apk', src: 'app-debug'} },
-    { text: 'Re-enable both Play Protect toggles', img: '' },
-    { text: 'Open the "Hisaab" app and allow SMS read permission', img: '' }
+    { text: 'Re-enable both Play Protect toggles', img: '5.jpeg' },
+    { text: 'Open the "Hisaab" app and allow SMS read permission', img: '7.jpeg' }
   ];
 
   usageSteps = [
